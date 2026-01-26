166f661898e62a253f85d9c8f39eb8dca2e3179b
chayan <chayanj39@gmail.com>
2026-01-26 13:57:57 +0530

    added hosted frontned to the cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://blood-aid-rw.vercel.app/",
     credentials: true
 }))
 app.use(cookieParser())
